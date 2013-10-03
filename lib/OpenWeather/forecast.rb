caa5bda77674f543c7c0ccb8357f36b107595f09
Chris911 <Christophe.naud.dulude@gmail.com>
2013-10-03 17:59:26 -0400

    Refactor min/max functions

--- lib/OpenWeather/forecast.rb
+++ lib/OpenWeather/forecast.rb
@@ -6,26 +6,14 @@ module OpenWeather
 
     def forecast_tomorrow_min(city)
       forecast = forecast_raw(city)
-      min_temp = 9999999
-      forecast['list'].each do |weather|
-        day = weather['dt_txt'].split(" ")[0]
-        next if day == today
-        break if day > tomorrow
-        min_temp = weather['main']['temp_min'] if weather['main']['temp_min'] < min_temp
-      end
-      min_temp
+      tomorrow_list = forecast['list'].select { |weather| weather['dt_txt'].split(" ")[0] == tomorrow  }
+      tomorrow_list.min_by { |temp| temp['main']['temp_min'] }['main']['temp_min']
     end
 
     def forecast_tomorrow_max(city)
       forecast = forecast_raw(city)
-      max_temp = -9999999
-      forecast['list'].each do |weather|
-        day = weather['dt_txt'].split(" ")[0]
-        next if day == today
-        break if day > tomorrow
-        max_temp = weather['main']['temp_max'] if weather['main']['temp_max'] > max_temp
-      end
-      max_temp
+      tomorrow_list = forecast['list'].select { |weather| weather['dt_txt'].split(" ")[0] == tomorrow  }
+      tomorrow_list.max_by { |temp| temp['main']['temp_max'] }['main']['temp_max']
     end
 
   end
