9b38858bdc5d92947d12b01597e762eafc39b6e6
Chris911 <Christophe.naud.dulude@gmail.com>
2013-10-03 18:24:12 -0400

    Adding rain? function

--- lib/OpenWeather/forecast.rb
+++ lib/OpenWeather/forecast.rb
@@ -16,5 +16,13 @@ module OpenWeather
       tomorrow_list.max_by { |temp| temp['main']['temp_max'] }['main']['temp_max']
     end
 
+    def forecast_tomorrow_rain?(city)
+      forecast = forecast_raw(city)
+      tomorrow_list = forecast['list'].select { |weather| weather['dt_txt'].split(" ")[0] == tomorrow  }
+      # This selects any period of time during the day where the condition code is between 500 and 599 which
+      # corresponds to rainy condition. If the array is empty that means it won't rain so we return the opposite.
+      ! tomorrow_list.select { |code| Integer(code['weather'][0]['id']).between?(500, 599) }.empty?
+    end
+
   end
 end
